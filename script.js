a20dc4dadf6cac09e6e2c420d0820e66a55df9c0
Philip Winchester <philipwinc@gmail.com>
2016-12-23 17:03:59 -0500

    added animation for text description

--- script.js
+++ script.js
@@ -1,22 +1,54 @@
 'use strict';
 (() => {
-  let loc1 = document.querySelector('.loc1');
-  let loc2 = document.querySelector('.loc2');
-  let loc3 = document.querySelector('.loc3');
   let char = document.querySelector('.char');
+  let loc1Title = document.querySelectorAll('h4')[0];
+  let loc2Title = document.querySelectorAll('h4')[1];
+  let loc3Title = document.querySelectorAll('h4')[2];
 
-
-  loc1.addEventListener('click', () => {
+  document.querySelector('.loc1').addEventListener('click', () => {
     char.className = "char char-loc1"
+    loc1Title.className = ".show-title"
+    wipeTitles([1,2])
   })
-  loc2.addEventListener('click', () => {
+  document.querySelector('.loc2').addEventListener('click', () => {
     char.className = "char char-loc2"
+    loc2Title.className = ".show-title"
+    wipeTitles([0,2])
   })
-  loc3.addEventListener('click', () => {
+  document.querySelector('.loc3').addEventListener('click', () => {
     char.className = "char char-loc3"
+    loc3Title.className = ".show-title"
+    wipeTitles([0,1])
   })
   document.querySelector('#start-tour').addEventListener('click', () => {
     char.className = "char char-animation"
+    wipeTitles([0,1,2])
   })
+
+  function wipeTitles(selArr) {
+    let hide1 = false
+    let hide2 = false
+    let hide3 = false
+    for (let i = 0; i < selArr.length; i++) {
+      if (selArr[i] === 0){
+        hide1 = true
+      }
+      if (selArr[i] === 1){
+        hide2 = true
+      }
+      if (selArr[i] === 2){
+        hide3 = true
+      }
+    }
+    if (hide1){
+      loc1Title.className = "hide-title"
+    }
+    if (hide2){
+      loc2Title.className = "hide-title"
+    }
+    if (hide3){
+      loc3Title.className = "hide-title"
+    }
+  }
 
 })();
