0b25f54bf339b122749da910f7dd4974fb85338f
Philip Winchester <philipwinc@gmail.com>
2016-12-23 15:33:24 -0500

    added tour and styling for easy layout help

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 'use strict';
-
 (() => {
-  console.log('script loaded');
+  let loc1 = document.querySelector('.loc1');
+  let loc2 = document.querySelector('.loc2');
+  let loc3 = document.querySelector('.loc3');
+  let char = document.querySelector('.char');
+
+
+  loc1.addEventListener('click', () => {
+    char.className = "char char-loc1"
+  })
+  loc2.addEventListener('click', () => {
+    char.className = "char char-loc2"
+  })
+  loc3.addEventListener('click', () => {
+    char.className = "char char-loc3"
+  })
+  document.querySelector('#start-tour').addEventListener('click', () => {
+    char.className = "char char-animation"
+  })
+
 })();
